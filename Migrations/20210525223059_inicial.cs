0f53031149a8bff050b068920dc74a6aa798a2dd
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-25 19:11:17 -0500

    Migraciones

---
+++ Migrations/20210525223059_inicial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticaN3.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCategoria = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProducto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Foto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LugarCompra = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NombreComprador = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    categoriaid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Productos_Categorias_categoriaid",
+                        column: x => x.categoriaid,
+                        principalTable: "Categorias",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_categoriaid",
+                table: "Productos",
+                column: "categoriaid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ Migrations/20210525231154_producto_sp.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticaN3.Migrations
+{
+    public partial class producto_sp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Categorias_categoriaid",
+                table: "Productos");
+
+            migrationBuilder.RenameColumn(
+                name: "categoriaid",
+                table: "Productos",
+                newName: "CategoriaId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Productos_categoriaid",
+                table: "Productos",
+                newName: "IX_Productos_CategoriaId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Productos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Categorias_CategoriaId",
+                table: "Productos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Categorias_CategoriaId",
+                table: "Productos");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoriaId",
+                table: "Productos",
+                newName: "categoriaid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Productos_CategoriaId",
+                table: "Productos",
+                newName: "IX_Productos_categoriaid");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "categoriaid",
+                table: "Productos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Categorias_categoriaid",
+                table: "Productos",
+                column: "categoriaid",
+                principalTable: "Categorias",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
