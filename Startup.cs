32f23e80b814ea45e600fcda750b8d60ebf5709b
Julio Ucharima <julio_ucharima@usmp.pe>
2021-05-25 19:12:00 -0500

    Conexion BD

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PracticaN3.Data;
 
 namespace PracticaN3
 {
@@ -24,6 +26,7 @@ namespace PracticaN3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BuscoContext>(dco =>  dco.UseSqlServer("Server=.;Database=Busco;Integrated Security=true;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
